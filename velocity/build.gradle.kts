f9b31b68f50c4aa82d97269664736dd3e7d37162
Excel619 <excel8392@gmail.com>
2025-12-16 09:41:10 -0700

    Switch to reposilite

--- velocity/build.gradle.kts
+++ velocity/build.gradle.kts
@@ -113,10 +113,10 @@ publishing {
     repositories {
         maven {
             name = "nexus"
-            url = uri("https://nexus.runicrealms.com/repository/maven-releases/")
+            url = uri("https://reposilite.runicrealms.com/releases/")
             credentials {
-                username = System.getenv("NEXUS_USERNAME")
-                password = System.getenv("NEXUS_PASSWORD")
+                username = System.getenv("REPOSILITE_USERNAME")
+                password = System.getenv("REPOSILITE_PASSWORD")
             }
         }
     }
